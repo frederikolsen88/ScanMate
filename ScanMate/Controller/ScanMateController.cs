4ae0cf12b55dc1c101f361fecb68425d8cddae94
Frederik Olsen <frederikolsen.dfl@gmail.com>
2014-12-06 11:15:21 +0100

    Added ScanMate source code

---
+++ ScanMate/Controller/ScanMateController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.itextpdf.text.pdf;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.Diagnostics;
+
+namespace ScanMate.Controller
+{
+    class ScanMateController
+    {
+        private static ScanMateController _instance = new ScanMateController();
+
+        private ScanMateController() { }
+
+        public static ScanMateController GetInstance
+        {
+            get 
+            { 
+                return _instance; 
+            }
+        }
+
+        public void HandleFileDrop(string[] fileList, bool deleteSource, bool split, bool reversePageOrder)
+        {
+            FileInfo pdfFile = new FileInfo(fileList[0]);
+
+            if (pdfFile.Extension != ".pdf")
+                return;
+                
+
+            if (split)
+                SplitPDF(pdfFile);
+
+            else if (reversePageOrder)
+                ReversePDFPageOrder(pdfFile);   
+
+            if (deleteSource)
+                File.Delete(pdfFile.FullName);
+        }
+
+        private void SplitPDF(FileInfo pdfFile)
+        {
+            PdfReader pdfReader = new PdfReader(pdfFile.FullName);
+
+            try
+            {
+                int pageCount = pdfReader.NumberOfPages;
+
+                for (int i = 1; i <= pageCount; i++)
+                {
+                    string output = pdfFile.FullName.Substring(0, pdfFile.FullName.IndexOf(".pdf")) + "_page" + i + ".pdf";
+
+                    Document document = new Document(pdfReader.GetPageSizeWithRotation(1));
+                    PdfCopy pdfWriter = new PdfCopy(document, File.Create(output));
+
+                    document.Open();
+
+                    PdfImportedPage page = pdfWriter.GetImportedPage(pdfReader, i);
+                    pdfWriter.AddPage(page);
+
+                    document.Close();
+                }
+            }
+
+            catch (Exception)
+            {
+                return;
+            }
+
+            finally
+            {
+                pdfReader.Close();
+            }
+        }
+
+        private void ReversePDFPageOrder(FileInfo pdfFile)
+        {
+            PdfReader pdfReader = new PdfReader(pdfFile.FullName);
+            Document document = new Document(pdfReader.GetPageSizeWithRotation(1));
+            string output = pdfFile.FullName.Substring(0, pdfFile.FullName.IndexOf(".pdf")) + "_reverse.pdf";
+
+            PdfCopy pdfWriter = new PdfCopy(document, File.Create(output));
+
+            try
+            {
+                int pageCount = pdfReader.NumberOfPages;
+                document.Open();
+
+                for (int i = pageCount; i >= 1; i--)
+                {
+                    PdfImportedPage page = pdfWriter.GetImportedPage(pdfReader, i);
+                    pdfWriter.AddPage(page);       
+                }
+
+                document.Close();
+            }
+
+            catch (Exception)
+            {
+                return;
+            }
+
+            finally
+            {
+                pdfReader.Close();
+                pdfWriter.Close();
+            }
+        }
+    }
+}
---
+++ ScanMate/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using ScanMate.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScanMate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BackgroundWorker bwScanMate = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bwScanMate.DoWork += bwScanMate_DoWork;
+            bwScanMate.RunWorkerCompleted += bwScanMate_RunWorkerCompleted;
+        }
+
+        private void imgPDF_Drop(object sender, DragEventArgs e)
+        {
+            try
+            { 
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                    if (files.Length > 1)
+                        throw new Exception("Please drop only one PDF file at a time!");
+
+                    object[] bwArgs = { files, chkBoxDelSrc.IsChecked.Value, btnSplitPDF.IsChecked.Value, btnReversePageOrder.IsChecked.Value };
+
+                    Mouse.OverrideCursor = Cursors.Wait;
+                    bwScanMate.RunWorkerAsync(bwArgs);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void bwScanMate_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object[] args = (object[])e.Argument;
+            string[] files = (string[])args[0];
+            bool delSrc = (bool)args[1];
+            bool splitPDF = (bool)args[2];
+            bool reversePageOrder = (bool)args[3];
+
+            ScanMateController.GetInstance.HandleFileDrop(files, delSrc, splitPDF, reversePageOrder);   
+        }
+
+        private void bwScanMate_RunWorkerCompleted(object sender,
+                                               RunWorkerCompletedEventArgs e)
+        {
+            Mouse.OverrideCursor = null;
+        }
+    }
+}
